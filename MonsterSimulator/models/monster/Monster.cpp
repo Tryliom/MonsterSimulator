6f00bc34cc774437be7614214b2bbe9482ada743
Alexis Haldy <alexishaldy@gmail.com>
2022-09-29 16:32:22 +0200

    Add attack func and attack stat to monster

--- MonsterSimulator/models/monster/Monster.cpp
+++ MonsterSimulator/models/monster/Monster.cpp
@@ -1,11 +1,24 @@
 #include "Monster.h"
 
-Monster::Monster(Race race, const int hp, const int armor, const int speed) : _race(std::move(race))
+Monster::Monster(Race race, const int hp, const int attack, const int armor, const int speed) : _race(std::move(race))
 {
 	_hp = hp;
 	_maxHp = hp;
+	_attack = attack;
 	_armor = armor;
 	_speed = speed;
+}
+
+void Monster::Attack(Monster* monster) const
+{
+	int damage = monster->GetArmor() - _attack;
+
+	if (damage > 0)
+	{
+		damage = 0;
+	}
+
+	monster->UpdateHp(damage);
 }
 
 
--- MonsterSimulator/models/monster/Monster.h
+++ MonsterSimulator/models/monster/Monster.h
@@ -7,16 +7,22 @@ private:
 	Race _race;
 	int _hp;
 	int _maxHp;
+	int _attack;
 	int _armor;
 	int _speed;
 
 public:
-	Monster(Race race, int hp, int armor, int speed);
+	Monster(Race race, int hp, int attack, int armor, int speed);
 
 	Race GetRace() const { return _race; }
 	int GetHp() const { return _hp; }
+	int GetAttack() const { return _attack; }
 	int GetMaxHp() const { return _maxHp; }
 	int GetArmor() const { return _armor; }
 	int GetSpeed() const { return _speed; }
+	void UpdateHp(const int hp) { _hp += hp; }
+
+	void Attack(Monster* monster) const;
+	bool IsDead() const { return _hp <= 0; }
 };
 
