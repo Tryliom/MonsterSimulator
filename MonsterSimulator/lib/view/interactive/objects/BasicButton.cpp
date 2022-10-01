e1a9069de69da5103946bed6a56fa77496b45597
Tryliom <alexishaldy@gmail.com>
2022-10-01 12:24:52 +0200

    Move Basic Button into his own file

---
+++ MonsterSimulator/lib/view/interactive/objects/BasicButton.cpp
@@ -0,0 +1,51 @@
+#include "BasicButton.h"
+
+namespace Console
+{
+	BasicButton::BasicButton(const std::string& str, const int x, const int y, const std::function<void()>& onClick, const bool xCentered, const bool yCentered) :
+		InteractiveObject(x, y, xCentered)
+	{
+		_str = str;
+		_yCentered = yCentered;
+		_onClick = onClick;
+	}
+
+	void BasicButton::Draw(Screen screen, const bool selected)
+	{
+		auto background = _background;
+		auto foreground = _foreground;
+
+		if (selected)
+		{
+			background = _selectedBackground;
+			foreground = _selectedForeground;
+		}
+
+		int y = _y;
+		if (_yCentered)
+		{
+			y -= 1;
+		}
+
+		// Draw a border around the button
+		std::string border;
+		for (int i = 0; i < static_cast<int>(_str.length()) + 2; i++)
+		{
+			border += _rowBorder;
+		}
+
+		std::string text = _columnBorder + _str + _columnBorder;
+
+		screen.Draw(Text{ .Str = border, .X = _x, .Y = y, .XCentered = _xCentered, .Background = background, .Foreground = foreground });
+		screen.Draw(Text{ .Str = text, .X = _x, .Y = y + 1, .XCentered = _xCentered, .Background = background, .Foreground = foreground });
+		screen.Draw(Text{ .Str = border, .X = _x, .Y = y + 2, .XCentered = _xCentered, .Background = background, .Foreground = foreground });
+	}
+
+	void BasicButton::OnKeyPress(const int key)
+	{
+		if (key == Key::Enter)
+		{
+			_onClick();
+		}
+	}
+}
---
+++ MonsterSimulator/lib/view/interactive/objects/BasicButton.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "../InteractiveObject.h"
+
+#include <functional>
+
+namespace Console
+{
+	class BasicButton : public InteractiveObject
+	{
+	private:
+		std::string _str{};
+		bool _yCentered{ false };
+		std::function<void()> _onClick{};
+		std::string _rowBorder{ "|" };
+		std::string _columnBorder{ "-" };
+
+	public:
+		BasicButton(const std::string& str, int x, int y, const std::function<void()>& onClick, bool xCentered = false, bool yCentered = false);
+
+		void SetOnClick(std::function<void()> onClick) { _onClick = std::move(onClick); }
+		void Draw(Screen screen, bool selected) override;
+		void OnKeyPress(const int key) override;
+	};
+}
+
