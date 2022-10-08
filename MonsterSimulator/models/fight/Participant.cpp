e0468eb98bbd03b944834f2bbcbbe15cedf167fb
Tryliom <alexishaldy@gmail.com>
2022-10-08 16:42:05 +0200

    Fix case where the hp bar is on the sprite
    Accelerate attack animation

--- MonsterSimulator/models/fight/Participant.cpp
+++ MonsterSimulator/models/fight/Participant.cpp
@@ -11,7 +11,7 @@ Participant::Participant(Monster* monster, bool isLeft)
 	_x = isLeft ? PositionX(0.25f) : PositionX(0.75f);
 	_y = PositionY(-monster->GetRace().GetSprite().GetHeight() / 2, 0.7f);
 	_xBar = isLeft ? PositionX(0.25f + X_BAR_OFFSET) : PositionX(0.75f + X_BAR_OFFSET);
-	_yBar = PositionY(2, 0.7f);
+	_yBar = PositionY(0.8f);
 	_xCoefficient = isLeft ? 1 : -1;
 }
 
@@ -31,10 +31,10 @@ void Participant::PlayTurn(const MainController* mainController, Participant* op
 	// Choose to attack
 	_attackAnimation = Animation(
 		10.0f,
-		mainController->CurrentFPS
+		mainController->CurrentFPS / 2
 	);
 
-	Utility::sleep(530);
+	Utility::sleep(250);
 
 	const int damageDealt = _monster->Attack(opponent->GetMonster());
 
